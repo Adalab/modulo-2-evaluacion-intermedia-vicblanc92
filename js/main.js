7f201eca8f7037cc791344ff6adb2752eb28fd99
vicblanc92 <vicbr92@gmail.com>
2021-12-14 10:28:18 +0100

    js structure, functions, const and listener

--- js/main.js
+++ js/main.js
@@ -1,1 +1,51 @@
+('use strict');
+console.log('Holis');
+//1.Variables globales :querySelector
 
+const selectMove = document.querySelector('.js-selector');
+const playButton = document.querySelector('.js-button');
+const userPoints = document.querySelector('.js-user');
+const computerPoints = document.querySelector('.js-computer');
+const letsGo = document.querySelector('.js-playGame');
+
+//2.Funciones
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+function numberMovement() {
+  //Generar número aleatorio
+  const randomNum = getRandomNumber(10);
+  //comprobar si es piedra, papel o tijera
+  if (randomNum < 3) {
+    console.log('Piedra');
+  } else if (randomNum >= 6) {
+    console.log('Papel');
+  } else {
+    console.log('Tijera');
+  }
+}
+
+// function piedraPapelTijera() {
+//   if () {
+//   }
+// }
+
+function pointsUser() {
+  if (userPoints === computerPoints) {
+    letsGo.innerHTML = 'EMPATE';
+  } else if (userPoints > computerPoints) {
+    letsGo.innerHTML = 'Has ganado';
+  } else computerPoints > userPoints;
+  letsGo.innerHTML = 'Has perdido';
+}
+
+function handlerClickPlay(event) {
+  event.preventDefault();
+  numberMovement();
+  pointsUser();
+}
+
+//3. Código que se ejecuta cuando se carga la página
+
+playButton.addEventListener('click', handlerClickPlay);
