ec1bc1b9b7e08150cec744bfd3383899063cbd5b
vicblanc92 <vicbr92@gmail.com>
2021-12-14 10:59:55 +0100

    update functions and const

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ const playButton = document.querySelector('.js-button');
 const userPoints = document.querySelector('.js-user');
 const computerPoints = document.querySelector('.js-computer');
 const letsGo = document.querySelector('.js-playGame');
+let inputComputer = document.querySelector('.js-inputComputer');
+let inputUser = document.querySelector('.js-inputUser');
 
 //2.Funciones
 function getRandomNumber(max) {
@@ -19,10 +21,13 @@ function numberMovement() {
   //comprobar si es piedra, papel o tijera
   if (randomNum < 3) {
     console.log('Piedra');
+    return 'Piedra';
   } else if (randomNum >= 6) {
     console.log('Papel');
+    return 'Papel';
   } else {
     console.log('Tijera');
+    return 'Tijera';
   }
 }
 
@@ -31,19 +36,36 @@ function numberMovement() {
 //   }
 // }
 
-function pointsUser() {
+function pointsUser(computerPoints) {
+  const userPoints = selectMove.value;
+
   if (userPoints === computerPoints) {
     letsGo.innerHTML = 'EMPATE';
-  } else if (userPoints > computerPoints) {
+  } else if (userPoints === 'Piedra' && computerPoints === 'Tijera') {
     letsGo.innerHTML = 'Has ganado';
-  } else computerPoints > userPoints;
-  letsGo.innerHTML = 'Has perdido';
+  } else if (userPoints === 'Piedra' && computerPoints === 'Papel') {
+    letsGo.innerHTML = 'Has perdido';
+  } else if (userPoints === 'Papel' && computerPoints === 'Piedra') {
+    letsGo.innerHTML = 'Has ganado';
+  } else if (userPoints === 'Papel' && computerPoints === 'Tijera') {
+    letsGo.innerHTML = 'Has perdido';
+  } else if (userPoints === 'Tijera' && computerPoints === 'Piedra') {
+    letsGo.innerHTML = 'Has perdido';
+  } else if (userPoints === 'Tijera' && computerPoints === 'Papel') {
+    letsGo.innerHTML = 'Has ganado';
+  }
+}
+
+function addPoints() {
+  let plusPointsComputer = (inputComputer += 1);
+  let plusPointsUser = (inputUser += 1);
 }
 
 function handlerClickPlay(event) {
   event.preventDefault();
-  numberMovement();
-  pointsUser();
+  const computer = numberMovement();
+  pointsUser(computer);
+  addPoints();
 }
 
 //3. Código que se ejecuta cuando se carga la página
